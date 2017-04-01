d8187e6c34df9f4cd46cfb18c3277f35f7c5b0dd
Alexey Park <alexey.g.park@gmail.com>
2017-04-01 18:45:33 +0600

    Devise update fixed

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -22,7 +22,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :full do
-    process resize_to_fit: [400, 400]
+    process resize_to_fit: [500, 500]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
